a2ac134479f982caec686e9d3f0c8ffc13ec7d8e
manukuiawa <manuelladefatima166@gmail.com>
2025-12-14 12:42:44 -0300

    Adicionando Lombok para os getters e setters

--- src/main/java/br/com/manuellakuiawa/todolist/user/UserModel.java
+++ src/main/java/br/com/manuellakuiawa/todolist/user/UserModel.java
@@ -1,31 +1,10 @@
 package br.com.manuellakuiawa.todolist.user;
 
+import lombok.Data;
+
+@Data //para getters e setters
 public class UserModel {
     private String username; 
     private String name; 
     private String password;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
