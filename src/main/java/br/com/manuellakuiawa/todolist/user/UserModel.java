ab0baf8d007546e9d3b3847b0e229e71fc7cdbe2
manukuiawa <manuelladefatima166@gmail.com>
2025-12-21 22:54:24 -0300

    Criando a tabela para o banco de dados

--- src/main/java/br/com/manuellakuiawa/todolist/user/UserModel.java
+++ src/main/java/br/com/manuellakuiawa/todolist/user/UserModel.java
@@ -1,10 +1,29 @@
 package br.com.manuellakuiawa.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data //para getters e setters
+
+//Criação tabela do banco de dados
+@Entity(name="tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id; //chave primária
+
     private String username; 
     private String name; 
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
 }
