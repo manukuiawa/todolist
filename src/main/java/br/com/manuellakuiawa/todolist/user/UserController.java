b6de83a52ebb9d662d8c090a00c8ff5be1b5fed1
manukuiawa <manuelladefatima166@gmail.com>
2025-12-22 21:47:05 -0300

    Ajustando o Controller para adicionar usuários no banco de dados

--- src/main/java/br/com/manuellakuiawa/todolist/user/UserController.java
+++ src/main/java/br/com/manuellakuiawa/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 package br.com.manuellakuiawa.todolist.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+
     }
 }
