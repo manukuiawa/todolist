e92a54fcc97ddd6180718bec359ae706e63b4e16
manukuiawa <manuelladefatima166@gmail.com>
2026-01-01 23:25:12 -0300

    Definindo getters e setters e adicionando quantidade de caracteres na tabela de tarefas do bd

--- src/main/java/br/com/manuellakuiawa/todolist/task/TaskModel.java
+++ src/main/java/br/com/manuellakuiawa/todolist/task/TaskModel.java
@@ -3,16 +3,36 @@ package br.com.manuellakuiawa.todolist.task;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
 public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
     private UUID id; 
     private String description; 
+
+    @Column(length = 50) //limitando 50 caracteres para o titulo
     private String title; 
+
     private LocalDateTime startAt; //horário de início 
     private LocalDateTime endAt; //horário de fim
     private String priority; 
 
+    private UUID idUser;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
     
-    private UUID idUser; 
     
+     
+
 }
