364c92878e4139da87572ee8217858e068235680
manukuiawa <manuelladefatima166@gmail.com>
2026-01-02 23:06:41 -0300

    Criação dos arquivos para as tasks

---
+++ src/main/java/br/com/manuellakuiawa/todolist/task/TaskController.java
@@ -0,0 +1,13 @@
+package br.com.manuellakuiawa.todolist.task;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    public void create(@RequestBody TaskModel taskModel)  {
+
+    }   
+}
