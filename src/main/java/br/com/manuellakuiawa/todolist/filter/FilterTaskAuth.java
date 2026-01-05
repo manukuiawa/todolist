126f8628dd536501a671ae7dc59346d15f0feead
manukuiawa <manuelladefatima166@gmail.com>
2026-01-05 20:37:04 -0300

    Implementando uma substring para ler o conteúdo

--- src/main/java/br/com/manuellakuiawa/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/manuellakuiawa/todolist/filter/FilterTaskAuth.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,12 +16,16 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+
         //pegar a autenticação (usuario e senha)
         var authorization = request.getHeader("Authorization");
-        System.out.println("Authorization");
-        System.out.println(authorization);
-        //validar usuário 
 
+        var user_password = authorization.substring("Basic".length()).trim(); //extrair conteudo (basic)
+        System.out.println("Authorization");
+        System.out.println(user_password);
+
+        //validar usuário 
+        
         //validar senha 
 
         filterChain.doFilter(request, response);
