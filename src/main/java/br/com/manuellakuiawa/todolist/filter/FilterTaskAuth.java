5ca32a52c70905bba194d2bd0bbd602d004681ee
manukuiawa <manuelladefatima166@gmail.com>
2026-01-05 21:01:23 -0300

    Implementando a condição de validação da senha

--- src/main/java/br/com/manuellakuiawa/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/manuellakuiawa/todolist/filter/FilterTaskAuth.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import br.com.manuellakuiawa.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -44,8 +45,15 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         } else {
 
             // validar senha
+            var passwordVerify =  BCrypt.verifyer() .verify(password.toCharArray(), user.getPassword());
+            
+            if(passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
 
-            filterChain.doFilter(request, response);
+            
         }
 
     }
