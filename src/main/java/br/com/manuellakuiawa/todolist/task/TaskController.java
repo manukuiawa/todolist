9b7380d2cde350ef4758f5d1ad68415e393f4bfa
manukuiawa <manuelladefatima166@gmail.com>
2026-01-10 23:56:39 -0300

    Implementando a condição para não deixar cadastrar uma data que já passou

--- src/main/java/br/com/manuellakuiawa/todolist/task/TaskController.java
+++ src/main/java/br/com/manuellakuiawa/todolist/task/TaskController.java
@@ -1,8 +1,11 @@
 package br.com.manuellakuiawa.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +21,17 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request)  {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request)  {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID)idUser);
 
         //Para não conseguir criar uma tarefa com a data que já passou
-        var currentDate = localDateTime.now();
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de ìnicio deve ser maior do que a data atual");
+        }
 
         var task = this.taskRepository.save(taskModel);
-        return task; 
+        return ResponseEntity.status(HttpStatus.OK).body(task); 
     }   
 }
