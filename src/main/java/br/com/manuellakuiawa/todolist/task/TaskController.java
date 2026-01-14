0032fe7a9aec6c18f53f408480a653a5d1a464b6
manukuiawa <manuelladefatima166@gmail.com>
2026-01-14 00:53:44 -0300

    Mpetods criados para fazer updates das tasks

--- src/main/java/br/com/manuellakuiawa/todolist/task/TaskController.java
+++ src/main/java/br/com/manuellakuiawa/todolist/task/TaskController.java
@@ -57,13 +57,13 @@ public class TaskController {
     public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
 
-        var task = this.taskRepository.findById(id);
+        var task = this.taskRepository.findById(id).orElse(null);
 
         Utils.copyNonNullProperties(taskModel, task);
 
         taskModel.setIdUser((UUID) idUser);
         taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        return this.taskRepository.save(task);
     }
 }
 
