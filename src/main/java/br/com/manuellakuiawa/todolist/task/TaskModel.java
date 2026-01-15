def9a0b4402ae01bf5a5fdf7c139774c1d25b87e
manukuiawa <manuelladefatima166@gmail.com>
2026-01-15 11:54:11 -0300

    Adicionando uma exceção para limite de caracteres

--- src/main/java/br/com/manuellakuiawa/todolist/task/TaskModel.java
+++ src/main/java/br/com/manuellakuiawa/todolist/task/TaskModel.java
@@ -17,22 +17,27 @@ public class TaskModel {
 
     @Id
     @GeneratedValue(generator = "UUID")
-    private UUID id; 
-    private String description; 
+    private UUID id;
+    private String description;
 
-    @Column(length = 50) //limitando 50 caracteres para o titulo
-    private String title; 
+    @Column(length = 50) // limitando 50 caracteres para o titulo
+    private String title;
 
-    private LocalDateTime startAt; //horário de início 
-    private LocalDateTime endAt; //horário de fim
-    private String priority; 
+    private LocalDateTime startAt; // horário de início
+    private LocalDateTime endAt; // horário de fim
+    private String priority;
 
     private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-    
-    
-     
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) {
+            throw new Exception("O campo title deve conter no máximo 50 caracteres");
+        }
+
+        this.title = title;
+    }
 
 }
